cc476f71ccdcd010b5f8d1f72850096e0ce8cec6
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-14 21:17:22 -0500

    went a different direction

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,86 @@
 var highScores = document.getElementById("high-scores");
 var timerDisplay = document.getElementById("countdown")
-var content = document.getElementById("content");
 var startPage = document.getElementById("start");
-var startBtn = document.getElementById("start-btn");
-
+var startBtn = document.getElementById("start-quiz");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById ("question");
+var options = document.getElementById ("options");
+var choiceA = document.getElementById ("A");
+var choiceB = document.getElementById ("B");
+var choiceC = document.getElementById ("C");
+var choiceD = document.getElementById ("D");
 var countDown = 60;
-var scores = loadScores() || [];
+
 var questions = [
     {
-        question    :
-        "What does HTML stand for?",
-        options     : [ "How Tall Monkeys Live", "Hypertext Markup Language", "Hottext Markup Lounge", "Hypertext Markup Lorem"],
-        correctAns  : 1
-    }
+        question    : "What does HTML stand for?",
+        choiceA     : "How Tall Monkeys Live", 
+        choiceB     : "Hypertext Markup Language", 
+        choiceC     : "Hottext Markup Lounge", 
+        choiceD     : "Hypertext Markup Lorem",
+        correctAns  : "B"
+    },
     {
-        question    :
-        "What does CSS stand for?",
-        options     : [ "Cascading Style Steps", "Cascading Simple Sheets", "Crappy Style Sheets", "Cascading Style Sheets"],
-        correctAns  : 3
-    }
+        question    : "What does CSS stand for?",
+        choiceA     : "Cascading Style Steps", 
+        choiceB     : "Cascading Simple Sheets", 
+        choiceC     : "Crappy Style Sheets", 
+        choiceD     : "Cascading Style Sheets",
+        correctAns  : "D"
+    },
     {
-        question    :
-        "What does JS stand for?",
-        options     : [ "Javascript", "Jambascript", "Jukescript", "Java"],
-        correctAns  : 0
-    }
+        question    : "What does JS stand for?",
+        choiceA     : "Javascript", 
+        choiceB     : "Jambascript", 
+        choiceC     : "Jukescript", 
+        choiceD     : "Java",
+        correctAns  : "A"
+    },
     {
-        question    :
-        "What does DOM stand for?",
-        options     : [ "Dudes Object Model", "Document Override Model", "Document Object Model", "Document Object Modal"],
-        correctAns  : 2
+        question    : "What does DOM stand for?",
+        choiceA     : "Dudes Object Model", 
+        choiceB     : "Document Override Model", 
+        choiceC     : "Document Object Model", 
+        choiceD     : "Document Object Modal",
+        correctAns  : "C"
     }
-]
+];
+var questionStart = 0;
+var lastQuestion = questions.length - 1;
+
+
+
+function createQuestion() {
+    let q = questions[questionStart];
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+};
+
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    createQuestion();
+    timer();
+};
+
+function timer() {
+    var time = function() {
+        countDown--;
+        countDown> 0 ? (timerDisplay.innerHTML = countDown) : (timerDisplay.innerText = 0);
+        if (counter === 0) {
+            clearInterval(startTime);
+            gamerOver();
+        }
+    }
+}
+
+function checkAnswer(answer) {
+    if(answer === questions[questionStart].correctAns) {
+        alert("Correct!");
+    } else {
+        alert("Incorrect!");
+    }
+};
