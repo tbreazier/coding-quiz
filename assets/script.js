1f83a95fd4af2eb22d90620d027bf0056ca2f47d
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-14 23:11:45 -0500

    after tutor session

--- assets/script.js
+++ assets/script.js
@@ -4,83 +4,77 @@ var startPage = document.getElementById("start");
 var startBtn = document.getElementById("start-quiz");
 var quiz = document.getElementById("quiz");
 var question = document.getElementById ("question");
-var options = document.getElementById ("options");
-var choiceA = document.getElementById ("A");
-var choiceB = document.getElementById ("B");
-var choiceC = document.getElementById ("C");
-var choiceD = document.getElementById ("D");
+var choicesEl = document.getElementById("choices");
 var countDown = 60;
 
 var questions = [
     {
-        question    : "What does HTML stand for?",
-        choiceA     : "How Tall Monkeys Live", 
-        choiceB     : "Hypertext Markup Language", 
-        choiceC     : "Hottext Markup Lounge", 
-        choiceD     : "Hypertext Markup Lorem",
-        correctAns  : "B"
+        question    :  "What does DOM stand for?",  
+        choices     : ["Dude", "Docs", "object", "model"],
+        correctAns  : "Docs"  
     },
     {
-        question    : "What does CSS stand for?",
-        choiceA     : "Cascading Style Steps", 
-        choiceB     : "Cascading Simple Sheets", 
-        choiceC     : "Crappy Style Sheets", 
-        choiceD     : "Cascading Style Sheets",
-        correctAns  : "D"
-    },
-    {
-        question    : "What does JS stand for?",
-        choiceA     : "Javascript", 
-        choiceB     : "Jambascript", 
-        choiceC     : "Jukescript", 
-        choiceD     : "Java",
-        correctAns  : "A"
-    },
-    {
-        question    : "What does DOM stand for?",
-        choiceA     : "Dudes Object Model", 
-        choiceB     : "Document Override Model", 
-        choiceC     : "Document Object Model", 
-        choiceD     : "Document Object Modal",
-        correctAns  : "C"
+        question    :  "What does HTML stand for?",  
+        choices     : ["HTML", "H", "B", "D"],
+        correctAns  : "HTML"  
     }
 ];
-var questionStart = 0;
+var currentQuestion= 0;
 var lastQuestion = questions.length - 1;
 
-
+function questionClick () {
+    var answer = this.value
+    
+    if(answer === questions[currentQuestion].correctAns) {
+        alert("Correct!");
+        currentQuestion++
+    } else {
+        alert("Incorrect!");
+        currentQuestion++
+    }
+    createQuestion()
+        /*if (questionStart < lastQuestion) {
+            questionStart++;
+            createQuestion();
+        } else {
+            alert("Done");
+        }*/
+};
 
 function createQuestion() {
-    let q = questions[questionStart];
-    question.innerHTML = "<p>" + q.question + "</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
+    let q = questions[currentQuestion];
+    question.innerHTML = "<h2>" + q.question + "</h2>";
+    //console.log(q);
+    //we could unhide it here
+    choicesEl.innerHTML = ""
+    q.choices.forEach(function(choice) {
+       
+        //console.log(choice);
+        var opBtn = document.createElement('button');
+        console.log(opBtn);
+        opBtn.setAttribute("class", "choiceBtn");
+        opBtn.setAttribute("value", choice)
+        opBtn.textContent = choice;
+        opBtn.addEventListener("click", questionClick);
+        choicesEl.appendChild(opBtn);
+        
+    })
+    //what if something is going to hide it after this line
+    
 };
+
+
 
 startBtn.addEventListener("click", startQuiz);
 
 function startQuiz() {
     createQuestion();
     timer();
+    startPage.style.display = "none";
 };
 
 function timer() {
-    var time = function() {
-        countDown--;
-        countDown> 0 ? (timerDisplay.innerHTML = countDown) : (timerDisplay.innerText = 0);
-        if (counter === 0) {
-            clearInterval(startTime);
-            gamerOver();
-        }
-    }
+   
 }
 
-function checkAnswer(answer) {
-    if(answer === questions[questionStart].correctAns) {
-        alert("Correct!");
-    } else {
-        alert("Incorrect!");
-    }
-};
+
